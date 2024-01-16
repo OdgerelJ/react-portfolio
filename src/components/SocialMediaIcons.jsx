6434803ddf20d7fe1ac94fbce7dec6982c85b9dd
2D\odie.jamsranjav <odie.jamsranjav@slalom.com>
2024-01-16 16:54:16 -0600

    resume link added

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -1,40 +1,32 @@
 const SocialMediaIcons = () => {
-    return (
-      <div className="flex justify-center md:justify-start my-10 gap-7">
-        <a
-          className="hover:opacity-50 transition duration-500"
-          href="https://www.linkedin.com/in/odgerel-jamsranjav/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img alt="linkedin-link" src="../assets/linkedin.png" />
-        </a>
-        <a
-          className="hover:opacity-50 transition duration-500"
-          href="https://www.twitter.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img alt="twitter-link" src="../assets/twitter.png" />
-        </a>
-        <a
-          className="hover:opacity-50 transition duration-500"
-          href="https://github.com/OdgerelJ"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img alt="github-link" src="../assets/github-mark-white.png" />
-        </a>
-        <a
-          className="hover:opacity-50 transition duration-500"
-          href="https://www.instagram.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img alt="instagram-link" src="../assets/instagram.png" />
-        </a>
-      </div>
-    );
-  };
-  
-  export default SocialMediaIcons;
+  return (
+    <div className="flex justify-center md:justify-start my-10 gap-7">
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.linkedin.com/in/odgerel-jamsranjav/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="linkedin-link" src="../assets/linkedin.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://drive.google.com/file/d/1hIsczx5AAaDtTZq_IuCmHSi-DHfgiFUJ/view?usp=sharing"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="twitter-link" src="../assets/resume.png" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://github.com/OdgerelJ"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img alt="github-link" src="../assets/github-mark-white.png" />
+      </a>
+    </div>
+  );
+};
+
+export default SocialMediaIcons;
